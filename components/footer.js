a371a4a39ef3c98861f5d41558184f688d77445b
Armino Popp <devel@boioiong.com>
2020-05-23 13:15:36 +0300

    Desktop version almost ready

--- components/footer.js
+++ components/footer.js
@@ -5,34 +5,42 @@ export default class Footer extends React.Component {
     render() {
         return (
             <footer>
-                <div className="container">
-                    <h2>NEWSLETTER</h2>
-                    <p>To recieve tips on how to grow your community, sign up to our weekly newsletter. 
-                    <br />We’ll never send you spam or pass on your email address
+
+                <div className="row">
+                    <div className="col">
+                        <h2>NEWSLETTER</h2>
+                        <p>To receive tips on how to grow your community, sign up to our weekly newsletter.
+                    <br />We’ll never send you spam or pass on your email address.
                     </p>
-                    <form>
-                        <input type="email" name="email" id="email" />
-                        <button className="btn btn-normal btn-big">Subscribe</button>
-                    </form>
-                </div>
-                <div className="container">
-                    <img className="logo" src="/images/logo.svg" alt="Logo" />
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Mauris nulla quam, hendrerit lacinia vestibulum a, ultrices quis sem.
-                    </p>
-                    <ul>
-                        <li><img src="/images/icon-phone.svg" alt="Phone icon" /> <span>Phone: +1-543-123-4567</span></li>
-                        <li><img src="/images/icon-email.svg" alt="Email icon" /> <span>example@huddle.com</span></li>
-                    </ul>
-                </div>
-                <div className="social-media">
-                    <i className="fa fa-facebook"></i> <i className="fa fa-instagram"></i> <i className="fa fa-twitter"></i>
+                        <form>
+                            <input type="email" name="email" id="email" />
+                            <button className="btn btn-normal btn-big">Subscribe</button>
+                        </form>
+                    </div>
+                    <div className="col">
+                        <div className="container">
+                            <img className="logo" src="/images/logo.svg" alt="Logo" />
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Mauris nulla quam, hendrerit lacinia vestibulum a, ultrices quis sem.
+                            </p>
+                            <ul>
+                                <li><img src="/images/icon-phone.svg" alt="Phone icon" /> <span>Phone: +1-543-123-4567</span></li>
+                                <li><img src="/images/icon-email.svg" alt="Email icon" /> <span>example@huddle.com</span></li>
+                            </ul>
+                        </div>
+                        <div className="social-media">
+                            <i className="fa fa-facebook"></i> <i className="fa fa-instagram"></i> <i className="fa fa-twitter"></i>
+                        </div>
+                    </div>
                 </div>
 
-                <div className="attribution">
-                    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+                <div className="row">
+                    <div className="attribution">
+                        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
                     Coded by <a href="https://github.com/armino-dev" title="See Armino's profile on GitHub" target="_blank">Armino</a>.
-                </div>  
+                </div>
+                </div>
+
             </footer>
         )
     }
--- pages/index.js
+++ pages/index.js
@@ -12,41 +12,65 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main>   
+      <main>
         <h1>Build The Community Your Fans Will Love</h1>
-        <p>Huddle re-imagines the way we build communities. You have a voice, but so does 
+        <p>Huddle re-imagines the way we build communities. You have a voice, but so does
 your audience. Create connections with your users as you engage in genuine discussion.</p>
-        <a href="#" className="btn btn-normal btn-rounded">Get Started For Free</a>
+        <a href="#" className="btn btn-normal btn-rounded btn-shadow">Get Started For Free</a>
         <div className="container screen-mockups">
           <img src="/images/screen-mockups.svg" alt="Screen mockups" />
         </div>
         <div className="container">
-          <Card logo="/images/icon-communities.svg" stats="1.4k+" text="Communities Formed" />
-          <Card logo="/images/icon-messages.svg" stats="2.7m+" text="Messages Sent" />
+          <div className="row">
+            <div className="col w50 center">
+              <Card logo="/images/icon-communities.svg" stats="1.4k+" text="Communities Formed" />
+            </div>
+            <div className="col w50 center">
+              <Card logo="/images/icon-messages.svg" stats="2.7m+" text="Messages Sent" />
+            </div>
+          </div>
         </div>
         <div className="container top-section-1">
-          <img src="/images/illustration-grow-together.svg" alt="Grow Together Illustration" />
-          <h2>Grow Together</h2>
-          <p>Generate meaningful discussions with your audience and build a strong, loyal community. 
-            Think of the insightful conversations you miss out on with a feedback form. 
+          <div className="row">
+            <div className="col w50 center">
+              <img src="/images/illustration-grow-together.svg" alt="Grow Together Illustration" />
+            </div>
+            <div className="col w50 center">
+              <h2>Grow Together</h2>
+              <p>Generate meaningful discussions with your audience and build a strong, loyal community.
+              Think of the insightful conversations you miss out on with a feedback form.
           </p>
+            </div>
+          </div>
         </div>
         <div className="container flowing-conversation">
-          <img src="/images/illustration-flowing-conversation.svg" alt="Flowing Conversation Illustration" />
-          <h2>Flowing Conversations</h2>
-          <p>You wouldn't paginate a conversation in real life, so why do it online? 
-            Our threads have just-in-time loading for a more natural flow.
+          <div className="row">
+            <div className="col w50 center">
+              <img src="/images/illustration-flowing-conversation.svg" alt="Flowing Conversation Illustration" />
+            </div>
+            <div className="col w50 center">
+              <h2>Flowing Conversations</h2>
+              <p>You wouldn't paginate a conversation in real life, so why do it online?
+              Our threads have just-in-time loading for a more natural flow.
           </p>
+            </div>
+          </div>
         </div>
         <div className="container top-section-2">
-          <img src="/images/illustration-your-users.svg" alt="Your Users Illustration" />
-          <h2>Your Users</h2>
-          <p>It takes no time at all to integrate Huddle with your app's authentication solution. This means, 
+          <div className="row">
+            <div className="col w50 center">
+            <img src="/images/illustration-your-users.svg" alt="Your Users Illustration" />
+            </div>
+            <div className="col w50 center">
+            <h2>Your Users</h2>
+          <p>It takes no time at all to integrate Huddle with your app's authentication solution. This means,
 once signed in to your app, your users can start chatting immediately.</p>
+            </div>
+          </div>          
         </div>
         <div className="container community">
           <h2>Ready To Build Your Community?</h2>
-          <a href="#" className="btn btn-normal btn-rounded">Get Started For Free</a>
+          <a href="#" className="btn btn-normal btn-rounded btn-shadow">Get Started For Free</a>
         </div>
       </main>
       <Footer />
