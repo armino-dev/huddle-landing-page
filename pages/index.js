3e0df384aae72cc0d56ea973c81282cbf8e7ffac
Armino Popp <devel@boioiong.com>
2020-05-25 08:41:18 +0300

    Added custom document to include lang

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,7 @@ import Card from '../components/card'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <meta charset="UTF-8" />
+      <Head>        
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Frontend Mentor | Huddle Landing Page</title>
         <link rel="icon" href="/favicon.ico" />
